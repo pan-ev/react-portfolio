5a89e47c760501c8d3de69f632c755d26c993b39
epan <epan616@gmail.com>
2023-06-21 00:14:25 -0400

    Update About Me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
+import Photo from '../../images/portfolio_pic.jpg';
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
+      <img id="portfolio_pic" src={Photo} alt=""></img>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hi, my name is Evan Pan. I studied Industrial & Operations Engineering
+        at the University of Michigan. I'm currently a Solutions Architect at
+        Chain.io.
       </p>
     </div>
   );
